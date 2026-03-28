df1e0d81bb1361b6f585f95523732dec831d07bc
Axel Burgos <burgosaxel56@gmail.com>
2026-03-28 10:05:06 -0400

    Wire localhost to a separate Firebase dev project

--- firebase-config.js
+++ firebase-config.js
@@ -30,13 +30,13 @@
       measurementId: 'G-2EE5JTG813'
     },
     development: {
-      apiKey: 'REPLACE_WITH_DEV_FIREBASE_API_KEY',
-      authDomain: 'REPLACE_WITH_DEV_FIREBASE_AUTH_DOMAIN',
-      projectId: 'REPLACE_WITH_DEV_FIREBASE_PROJECT_ID',
-      storageBucket: 'REPLACE_WITH_DEV_FIREBASE_STORAGE_BUCKET',
-      messagingSenderId: 'REPLACE_WITH_DEV_FIREBASE_MESSAGING_SENDER_ID',
-      appId: 'REPLACE_WITH_DEV_FIREBASE_APP_ID',
-      measurementId: 'REPLACE_WITH_DEV_FIREBASE_MEASUREMENT_ID'
+      apiKey: 'AIzaSyAwVljXUklWbroQNYMIKrM6pC0uRQl-VcY',
+      authDomain: 'ebanisteriacad-dev.firebaseapp.com',
+      projectId: 'ebanisteriacad-dev',
+      storageBucket: 'ebanisteriacad-dev.firebasestorage.app',
+      messagingSenderId: '769455881533',
+      appId: '1:769455881533:web:af03ee7c0af33017a8d954',
+      measurementId: ''
     }
   };
 
